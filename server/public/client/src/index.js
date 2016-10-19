23da671ab4a02a77bf7ee94c99f1de91e5529390
Octav Zaharia <o.zaharia@levi9.com>
2016-10-19 17:17:49 +0300

    added redux-thunk

--- server/public/client/src/index.js
+++ server/public/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {render} from 'react-dom'
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 import {Provider} from 'react-redux'
 import reducer from './reducers'
 import 'todomvc-app-css/index.css'
@@ -8,13 +8,14 @@ import {Router, Route, IndexRoute, browserHistory} from 'react-router'
 import RegisterContainer from "./containers/RegisterContainer";
 import AppContainer from "./containers/AppContainer";
 import ItemsContainer from "./containers/ItemsContainer";
+import thunk from 'redux-thunk';
 
-const store = createStore(reducer);
+const store = createStore(reducer, thunk);
 
 render(
     <Provider store={store}>
         <Router history={browserHistory}>
-            <Route path="/" >
+            <Route path="/">
                 <IndexRoute component={AppContainer}/>
                 <Route path="login" component={AppContainer}/>
                 <Route path="register" component={RegisterContainer}/>
